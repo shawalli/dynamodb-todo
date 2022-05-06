0ff25467a095b27500c1a132e0091033f17a7c51
shawalli <shawn.p.wallis@gmail.com>
2022-05-06 19:32:07 +0000

    fix(api): only return body if provided

--- api/src/response.py
+++ api/src/response.py
@@ -2,20 +2,23 @@ import json
 from copy import deepcopy
 
 
-def make_response(lambda_execution_id, status=200, body=None):
-    # if body is None:
-    #     body = dict()
-    
-    body["id"] = deepcopy(lambda_execution_id)
+def is_error_response(status_code):
+    return 200 < status_code < 400
+
+
+def make_response(lambda_execution_id, status_code=200, body=None):
+    if not is_error_response(status_code):
+        if body is None:
+            body = dict()
+
+        body["id"] = deepcopy(lambda_execution_id)
 
     response = {
-        "statusCode": status,
+        "statusCode": status_code,
         "headers": {
-            # "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
         },
         "isBase64Encoded": False,
-        # "body": json.dumps(body),
     }
 
     if body is not None:
